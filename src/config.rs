5f125f54f15a1ad921d5d9550fa65b2d79d8ab87
poly000 <1348292515@qq.com>
2024-03-31 10:17:51 +0800

    feat: print received events

--- src/config.rs
+++ src/config.rs
@@ -4,6 +4,9 @@ use serde::Deserialize;
 pub struct Config {
     pub username: String,
     pub password: String,
+
     pub spam_limit: u32,
     pub spam_keywords: Vec<String>,
+    
+    pub http_proxy: Option<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ mod config;
 use config::Config;
 
 use crossbeam_skiplist::SkipMap;
+use matrix_sdk::{
+    config::SyncSettings,
+    ruma::{events::room::message::SyncRoomMessageEvent, UserId},
+    Client,
+};
 use std::{
     fs,
     sync::{atomic::AtomicUsize, Arc},
@@ -14,5 +19,28 @@ async fn main() -> Result<()> {
     let config: Arc<Config> = Arc::new(toml::from_str(&fs::read_to_string("config.toml")?)?);
     let spam_count_map: SkipMap<String, AtomicUsize> = SkipMap::new();
 
+    let client = build_client(&config).await?;
+
+    client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
+        println!("Received a message {ev:?}");
+    });
+
+    client.sync(SyncSettings::default()).await?;
+
     Ok(())
+}
+
+async fn build_client(config: &Config) -> Result<Client> {
+    let userid: &UserId = config.username.as_str().try_into()?;
+    let mut client = matrix_sdk::Client::builder().server_name(userid.server_name());
+    if let Some(proxy) = &config.http_proxy {
+        client = client.proxy(proxy);
+    }
+    let client = client.build().await?;
+    client
+        .matrix_auth()
+        .login_username(userid, &config.password)
+        .await?;
+
+    Ok(client)
 }
