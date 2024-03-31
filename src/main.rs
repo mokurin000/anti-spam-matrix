a470014b0a03f3e147b86470ad1fb6b1251c7a37
poly000 <1348292515@qq.com>
2024-03-31 11:02:28 +0800

    feat: initial implementation

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ use config::Config;
 use crossbeam_skiplist::SkipMap;
 use matrix_sdk::{
     config::SyncSettings,
-    ruma::{events::room::message::SyncRoomMessageEvent, UserId},
+    ruma::{
+        events::{
+            room::message::SyncRoomMessageEvent, OriginalSyncMessageLikeEvent, SyncMessageLikeEvent,
+        },
+        UserId,
+    },
     Client,
 };
 use std::{
@@ -17,12 +22,41 @@ use anyhow::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
     let config: Arc<Config> = Arc::new(toml::from_str(&fs::read_to_string("config.toml")?)?);
-    let spam_count_map: SkipMap<String, AtomicUsize> = SkipMap::new();
+    let spam_count_map: Arc<SkipMap<String, AtomicUsize>> = Arc::new(SkipMap::new());
 
     let client = build_client(&config).await?;
 
     client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
-        println!("Received a message {ev:?}");
+        let SyncMessageLikeEvent::Original(ev) = ev else {
+            return;
+        };
+
+        let OriginalSyncMessageLikeEvent {
+            content, sender, ..
+        } = ev;
+
+        if content.msgtype() != "m.text" {
+            return;
+        }
+
+        let body = content.msgtype.body();
+        if config.spam_keywords.iter().any(|key| body.contains(key)) {
+            spam_count_map
+                .get_or_insert(sender.to_string(), AtomicUsize::new(1))
+                .value()
+                .fetch_add(1, std::sync::atomic::Ordering::AcqRel);
+        } else {
+            spam_count_map.insert(sender.to_string(), AtomicUsize::new(1));
+        }
+
+        if spam_count_map
+            .get_or_insert(sender.to_string(), AtomicUsize::new(1))
+            .value()
+            .load(std::sync::atomic::Ordering::Acquire)
+            > config.spam_limit as _
+        {
+            println!("ban {sender}");
+        }
     });
 
     client.sync(SyncSettings::default()).await?;
